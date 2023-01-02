de9c5682e53785b61a29f805de6b29ded95772dd
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-01-02 13:23:55 +0200

    added transaction making functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TransactionProvider } from './context/TranscactionContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <TransactionProvider>
     <App />
-  </React.StrictMode>,
-)
+  </TransactionProvider>,
+);
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractAddress = '0x10f7D1cDbF88d5Fb027584C213Cf3Bfb8AB520Cc';
+export const contractABI = abi.abi;
